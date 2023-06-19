b134dc4c6a4c9562e41214367b4c587466e6d72d
Amir Qasemi <amirghasemi1375@gmail.com>
2023-06-19 15:08:39 +0330

    solve jum_game II

---
+++ leetcode/jum_game_ii.go
@@ -0,0 +1,17 @@
+func jump(nums []int) int {
+	numOfJumps := make([]int, len(nums))
+	numOfJumps[0] = 0
+	for i := 1; i < len(nums); i++ {
+		numOfJumps[i] = math.MaxInt
+	}
+
+	for i := 0; i < len(nums); i++ {
+		for j := 1; j <= nums[i]; j++ {
+			if i+j < len(nums) && numOfJumps[i]+1 < numOfJumps[i+j] {
+				numOfJumps[i+j] = numOfJumps[i] + 1
+			}
+		}
+	}
+
+	return numOfJumps[len(numOfJumps)-1]
+}
